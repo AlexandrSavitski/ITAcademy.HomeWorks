507c634bfd67b929398f615aac88129c2ed90db2
Alexandr <Alexandr@ALEXANDR->
2020-03-18 19:06:23 +0300

    HW05.Task2 One step with char. Done

--- blank/HW05.Task2.1/Program.cs
+++ blank/HW05.Task2.1/Program.cs
@@ -6,7 +6,35 @@ namespace HW05.Task2._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str = Console.ReadLine();
+            char strChar = Convert.ToChar(str);
+            bool wrong = true;
+
+            switch (strChar)
+            {
+                case 'w':
+                    str = " вперед";
+                    break;
+                case 'a':
+                    str = " влево";
+                    break;
+                case 's':
+                    str = " назад";
+                    break;
+                case 'd':
+                    str = " вправо";
+                    break;
+                default:
+                    str = "Вы ввели неверные данные";
+                    wrong = false;
+                    break;
+            }
+            
+            if (wrong)
+            {
+                Console.WriteLine($"Вы переместились{str}");
+            }
+            else Console.WriteLine("Вы ввели неверные данные");
         }
     }
 }
