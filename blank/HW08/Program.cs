823ab87294e9bc7cb1a192964629e71455ea8b15
Alexandr <Alexandr@ALEXANDR->
2020-04-01 23:26:21 +0300

    HW8 Done

---
+++ blank/HW08/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace HW08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str2;
+            string str3 = "1a!2.3!!.. 4.!.?6 7! ..?";
+            Task1 task1 = new Task1();
+            Task2 task2 = new Task2();
+            Task3 task3 = new Task3();
+
+            Console.WriteLine("Введите стих, разделяя строки \";\"");
+            task1.Poem(Console.ReadLine());
+            Console.WriteLine("Введите текст с очень длинным словом (самое длинное слово будет удалено)");
+            str2 = Console.ReadLine();
+            task2.DeleteLongWord(str2);
+            Console.WriteLine("Поменять местами длинное и короткое слово");
+            task2.ReplaceWord(str2);
+            task2.CountLetterSymbol(str2);
+            Console.WriteLine("Сортировка по убыванию");
+            task2.Sort(str2);
+            task3.ActionWithString(str3);
+        }
+    }
+}
---
+++ blank/HW08/Task2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW08
+{
+    class Task2
+    {
+        internal void DeleteLongWord(string str)
+        {
+            string[] strArr = StringToArray(str);
+            str = str.Remove(0, str.Length);
+            int maxlen = 0, index = 0;
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (strArr[i].Length > maxlen)
+                {
+                    maxlen = strArr[i].Length;
+                    index = i;
+                }
+            }
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (i != index) str += $"{strArr[i]} ";
+            }
+            Console.WriteLine(str = str.Trim());
+        }
+
+        internal void ReplaceWord(string str)
+        {
+            int tempMax = 0;
+            int tempMin = 0;
+            string temp;
+            string[] arr = StringToArray(str);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length > arr[tempMax].Length) tempMax = i;
+                if (arr[i].Length < arr[tempMin].Length) tempMin = i;
+            }
+            temp = arr[tempMax];
+            arr[tempMax] = arr[tempMin];
+            arr[tempMin] = temp;
+
+            str = str.Remove(0, str.Length);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                str += $"{arr[i]} ";
+            }
+            Console.WriteLine(str = str.Trim());
+        }
+
+        internal void CountLetterSymbol(string str)
+        {
+            int countSymbol = 0;
+            int countLetter = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsLetter(str[i])) countLetter++;
+                if (!Char.IsLetter(str[i]) && !str[i].Equals(' ')) countSymbol++;
+            }
+
+            Console.WriteLine($"Букв: {countLetter}");
+            Console.WriteLine($"Символов: {countSymbol}");
+        }
+
+        internal void Sort(string str)
+        {
+            string[] arr = StringToArray(str);
+            string temp;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i].Length < arr[j].Length)
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+
+        string[] StringToArray(string str)
+        {
+            string[] strArr = str.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return strArr;
+        }
+    }
+}
